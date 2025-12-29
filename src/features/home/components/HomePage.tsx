56645d66406b702317b321c8859e4393d7f3e229
dhengGhuring <wyvrendragon@gmail.com>
2025-12-29 08:03:24 +0700

    feat: Add Supabase client setup and implement basic user login functionality.
    issue number: #2

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { supabase } from '@/lib/supabase/client';
 import React from 'react'
 
 export const HomePage = () => {
+    const login = async () => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: "adminmasjid@masjidnurussholeh.com",
+            password: "password123"
+        })
+
+        if (error) {
+            console.error(error.message)
+        } else {
+            console.log("Login berhasil", data)
+        }
+    }
     return (
-        <div>HomePage</div>
+        <div>
+            <button onClick={login}>Login</button>
+        </div>
     )
 }
